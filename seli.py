3bb96e33e96362d91a0b341cd8e59031e17a940e
vinay-winai <60219349+vinay-winai@users.noreply.github.com>
2025-06-09 17:20:50 +0530

    Update seli.py

--- seli.py
+++ seli.py
@@ -5,38 +5,32 @@ from selenium.common.exceptions import NoSuchElementException, WebDriverExceptio
 import time
 
 with open('down.txt', 'r') as file:
-    links = file.readlines()[2:]
+    links = file.readlines()[1:]
 
 # Configure Firefox options for private browsing
+firefox_profile = r"C:\Users\user\AppData\Roaming\Mozilla\Firefox\Profiles\prof-id"
 firefox_options = Options()
-firefox_options.add_argument("-private")
+firefox_options.add_argument(f"--profile={firefox_profile}")
+firefox_options.add_argument("--headless")
+firefox_options.add_argument("--private-window")
 
 # Iterate over each link
 for link in links:
     link = link.strip()
+    link = link[2:]
     try:
         driver = webdriver.Firefox(options=firefox_options)
         driver.get(link)
-        
         # Allow the page to load
         time.sleep(5)
-        
-        # Try to find and click the first button 'x'
         try:
-            button = driver.find_element(By.XPATH, "//button[text()='x']")
-            button.click()
-            time.sleep(2)  # Wait to observe the effect
-            
-            # Try to find and click the button again
-            button.click()
-            time.sleep(2)  # Wait to observe the effect
-            
+            button = driver.find_element(By.XPATH, "//button[text()='DOWNLOAD']")
+            button.click()            
+            time.sleep(2)        
         except NoSuchElementException:
             print(f"Button 'x' not found on page: {link}")
-        
         # Close the tab
         driver.quit()
-    
     except WebDriverException as e:
         print(f"Failed to load the page or other browser error for link: {link}\nError: {e}")
 
